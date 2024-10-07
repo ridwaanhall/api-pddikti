dbe7c851633cddf1e2d201e3606cb4a49c5660f9
ridwaanhall <erbyl14@gmail.com>
2024-10-07 12:34:08 +0700

    Refactor URL configuration to include additional API endpoints for PDDIKTI API

--- pddikti_api/urls.py
+++ pddikti_api/urls.py
@@ -13,6 +13,23 @@ urlpatterns = [
     path('search/mhs/<str:keyword>/', views.SearchMahasiswa.as_view(), name='search-mahasiswa'),
     
     # pt
-    path('pt/detail/<str:id>/', views.PTDetail.as_view(), name='pt-detail'),
-    path('pt/prodi/<str:id>/<str:id_thsmt>', views.PTProdi.as_view(), name='pt-prodi'),
+    path('pt/detail/<str:id_pt>/', views.PTDetail.as_view(), name='pt-detail'),
+    path('pt/prodi/<str:id_pt>/<str:id_thsmt>', views.PTProdi.as_view(), name='pt-prodi'),
+    path('pt/rasio/<str:id_pt>/', views.PTRasioDosenMahasiswa.as_view(), name='pt-rasio'),
+    path('pt/mahasiswa/<str:id_pt>/', views.PTMeanLulusMaba.as_view(), name='pt-mahasiswa'),
+    path('pt/waktu-studi/<str:id_pt>/', views.PTMeanMasaStudi.as_view(), name='pt-waktu-studi'),
+    path('pt/riwayat/<str:id_pt>/', views.PTRiwayat.as_view(), name='pt-riwayat'),
+    path('pt/biaya-kuliah/<str:id_pt>/', views.PTBiayaKuliah.as_view(), name='pt-biaya-kuliah'),
+    path('pt/fasilitas/<str:id_pt>/', views.PTFasilitas.as_view(), name='pt-fasilitas'),
+    path('pt/logo/<str:id_pt>/', views.PTLogo.as_view(), name='pt-logo'),
+    
+    # prodi
+    path('prodi/detail/<str:id_prodi>/', views.ProdiDetail.as_view(), name='prodi-detail'),
+    path('prodi/desc/<str:id_prodi>/', views.ProdiDesc.as_view(), name='prodi-desc'),
+    path('prodi/num-students-lecturers/<str:id_prodi>/', views.ProdiJumlahMHSDosen.as_view(), name='prodi-num-students-lecturers'),
+    path('prodi/riwayat/<str:id_prodi>/', views.ProdiRiwayat.as_view(), name='prodi-riwayat'),
+    path('prodi/biaya-kuliah/<str:id_prodi>/', views.ProdiBiayaKuliah.as_view(), name='prodi-biaya-kuliah'),
+    path('prodi/dosen-homebase/<str:id_prodi>/<str:id_thsmt>/', views.ProdiDosenHomebase.as_view(), name='prodi-dosen-homebase'),
+    path('prodi/dosen-penghitung-ratio/<str:id_prodi>/<str:id_thsmt>/', views.ProdiDosenPenghitungRatio.as_view(), name='prodi-dosen-penghitung-ratio'),
+    
 ]
