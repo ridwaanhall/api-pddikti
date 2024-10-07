d4596aa7e74224da15842741d8c37140006a3b72
ridwaanhall <erbyl14@gmail.com>
2024-10-07 12:34:00 +0700

    Refactor views and add API endpoints for PDDIKTI API

--- pddikti_api/views.py
+++ pddikti_api/views.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.http import HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -31,6 +32,32 @@ def make_api_request_2(endpoint, id, id_thsmt):
         return response.json()
     except requests.exceptions.RequestException as e:
         return {'error': str(e)}
+    
+def make_api_request_3(endpoint, id, id_thsmt):
+    api_url = f"{BASE_URL}/{endpoint}/{id}?semester={id_thsmt}"
+    headers = {
+        'x-api-key': settings.API_KEY
+    }
+
+    try:
+        response = requests.get(api_url, headers=headers, timeout=10)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        return {'error': str(e)}
+    
+def make_api_request_img(endpoint, id):
+    api_url = f"{BASE_URL}/{endpoint}/{id}"
+    headers = {
+        'x-api-key': settings.API_KEY
+    }
+
+    try:
+        response = requests.get(api_url, headers=headers, timeout=10)
+        response.raise_for_status()
+        return response.content
+    except requests.exceptions.RequestException as e:
+        return None
 
 @api_view(['GET'])
 def api_overview(_):
@@ -66,11 +93,85 @@ class SearchMahasiswa(APIView):
     
 # PT
 class PTDetail(APIView):
-    def get(self, _, id):
-        data = make_api_request('pt/detail', id)
+    def get(self, _, id_pt):
+        data = make_api_request('pt/detail', id_pt)
         return Response(data)
 
 class PTProdi(APIView):
-    def get(self, _, id, id_thsmt):
-        data = make_api_request_2('pt/prodi', id, id_thsmt)
+    def get(self, _, id_pt, id_thsmt):
+        data = make_api_request_2('pt/prodi', id_pt, id_thsmt)
+        return Response(data)
+    
+class PTRasioDosenMahasiswa(APIView):
+    def get(self, _, id_pt):
+        data = make_api_request('pt/rasio', id_pt)
+        return Response(data)
+    
+class PTMeanLulusMaba(APIView):
+    def get(self, _, id_pt):
+        data = make_api_request('pt/mahasiswa', id_pt)
+        return Response(data)
+    
+class PTMeanMasaStudi(APIView):
+    def get(self, _, id_pt):
+        data = make_api_request('pt/waktu-studi', id_pt)
+        return Response(data)
+    
+class PTRiwayat(APIView):
+    def get(self, _, id_pt):
+        data = make_api_request('pt/name-histories', id_pt)
+        return Response(data)
+    
+class PTBiayaKuliah(APIView):
+    def get(self, _, id_pt):
+        data = make_api_request('pt/cost-range', id_pt)
+        return Response(data)
+    
+class PTFasilitas(APIView):
+    def get(self, _, id_pt):
+        data = make_api_request('pt/sarpras-file-name', id_pt)
+        return Response(data)
+    
+class PTLogo(APIView):
+    def get(self, request, id_pt):
+        img_data = make_api_request_img('pt/logo', id_pt)
+        if img_data:
+            return HttpResponse(img_data, content_type="image/png")
+        else:
+            return HttpResponse(status=404)
+        
+# prodi
+class ProdiDetail(APIView):
+    def get(self, _, id_prodi):
+        data = make_api_request('prodi/detail', id_prodi)
+        return Response(data)
+    
+class ProdiDesc(APIView):
+    def get(self, _, id_prodi):
+        data = make_api_request('prodi/desc', id_prodi)
+        return Response(data)
+    
+class ProdiJumlahMHSDosen(APIView):
+    def get(self, _, id_prodi):
+        data = make_api_request('prodi/num-students-lecturers', id_prodi)
+        return Response(data)
+    
+class ProdiRiwayat(APIView):
+    def get(self, _, id_prodi):
+        data = make_api_request('prodi/name-histories', id_prodi)
+        return Response(data)
+    
+class ProdiBiayaKuliah(APIView):
+    def get(self, _, id_prodi):
+        data = make_api_request('prodi/cost-range', id_prodi)
+        return Response(data)
+    
+class ProdiDosenHomebase(APIView):
+    def get(self, _, id_prodi, id_thsmt):
+        data = make_api_request_3('dosen/homebase', id_prodi, id_thsmt)
+        return Response(data)
+    
+class ProdiDosenPenghitungRatio(APIView):
+    def get(self, _, id_prodi, id_thsmt):
+        data = make_api_request_3('dosen/penghitung-ratio', id_prodi, id_thsmt)
         return Response(data)
