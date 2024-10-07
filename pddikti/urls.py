4b8ee3032a3d3e48997ae2146440a6f5fbb0ccb8
ridwaanhall <erbyl14@gmail.com>
2024-10-07 14:05:48 +0700

    Refactor URL patterns to redirect all unknown requests to the '/api/' endpoint

--- pddikti/urls.py
+++ pddikti/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('pddikti_api.urls')),
+    re_path(r'^.*/$', RedirectView.as_view(url='/api/', permanent=True)),
 ]
