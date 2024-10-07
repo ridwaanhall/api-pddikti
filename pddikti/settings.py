dcc9fe4a8f6e203882b660de264b3134317e07c3
ridwaanhall <erbyl14@gmail.com>
2024-10-07 13:24:13 +0700

    Refactor static file configuration

--- pddikti/settings.py
+++ pddikti/settings.py
@@ -49,10 +49,13 @@ INSTALLED_APPS = [
     
     'rest_framework',
     'pddikti_api',
+    "whitenoise.runserver_nostatic",
+    "django.contrib.staticfiles",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,6 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
