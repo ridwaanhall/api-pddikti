f38a308a107aa5672e5070836b8b4f539b914b33
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-10-27 05:26:18 +0000

    fix(settings): change default API availability to false for limited access

--- pddikti/settings.py
+++ pddikti/settings.py
@@ -28,7 +28,7 @@ RIDWAANHALL_KEY=config('RIDWAANHALL_KEY')
 RIDWAANHALL_HASH_KEY=config('RIDWAANHALL_HASH_KEY')
 
 # API Status Control
-API_AVAILABILITY = config('API_AVAILABILITY', default=True, cast=bool) # false is limited access. true is full access
+API_AVAILABILITY = config('API_AVAILABILITY', default=False, cast=bool) # false is limited access. true is full access
 API_VERSION = config('API_VERSION', default='3.0.4', cast=str)  # Default API version
 LAST_UPDATE = config('LAST_UPDATE', default='2025-08-22T15:04:00+07:00', cast=str)
 
